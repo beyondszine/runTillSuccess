12f201d85e993b80ca512bc4ad0db2c5eab0fd4a
beyondszine <saurabhshandilya.1991@gmail.com>
2019-04-22 21:20:36 +0530

    working

--- rts.sh
+++ rts.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 # constants for success value of your program, just in case its not standard '0'.  Delay between consecutive tries if it fails due to any reason.  m_result is random number given other than success value.
-m_result=100
-SUCCESS_VALUE=0
+m_result="100"
+SUCCESS_VALUE="0"
 DELAY_BETWEEN_TRIES_SECONDS=5
 
 
 if [ -z "$1" ]
 then
-echo "No command provided!"
+    echo "No command provided!"
 else
-echo "Your given command is: $1"
-echo "Running your given command till it gets succeed!"
+    echo "Your given command is: $1"
+    echo "Sequential command queued is $2"
+fi
 
-until [ $m_result -ne $SUCCESS_VALUE ]
+echo "Running your given command till it gets succeed!"
+echo $m_result,$SUCCESS_VALUE
+
+while [ "$m_result" -ne "$SUCCESS_VALUE" ]
 do
     eval $1
     m_result=$?
