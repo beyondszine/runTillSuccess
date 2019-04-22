46d6da26c76c1260fdb4a1d0256444d6ca4c8058
beyondszine <saurabhshandilya.1991@gmail.com>
2019-04-22 21:00:14 +0530

    initial commit

---
+++ rts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# constants for success value of your program, just in case its not standard '0'.  Delay between consecutive tries if it fails due to any reason.  m_result is random number given other than success value.
+m_result=100
+SUCCESS_VALUE=0
+DELAY_BETWEEN_TRIES_SECONDS=5
+
+
+if [ -z "$1" ]
+then
+echo "No command provided!"
+else
+echo "Your given command is: $1"
+echo "Running your given command till it gets succeed!"
+
+until [ $m_result -ne $SUCCESS_VALUE ]
+do
+    eval $1
+    m_result=$?
+    echo "Current return value yet not SUCCESS"
+    sleep $DELAY_BETWEEN_TRIES_SECONDS
+done
+echo "Command executed successfully!" 
+eval $2
